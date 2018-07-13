575af4d304421993e315f8225605b2cdfcfbb5d0
Maseeh Es-Haq <eshaqmaseeh@yahoo.com>
2018-07-12 23:03:37 -0400

    add body parser for post parsing, session, user routes

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')();
 const session = require("express-session");
-const mongoose = require("./db/models");
-// const variable = mongoose.model("");
+const passport = require('./passport');
 const cors = require("cors");
 const axios = require("axios");
+const users = require('./routes/users')
+const app = express();
 
 app.use(cors());
+
+// Cookie parser
+app.use(cookieParser)
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true })
+);
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
 
 // app.use(express.static('public'));
 
@@ -37,6 +59,8 @@ app.post("/search/:title", (req, res) => {
 			res.json(reply.data.recipes);
 		});
 });
+
+app.use('/users/', users)
 
 app.listen(3001, () => console.log("Server's running on 3001!"));
 // app.set('port', process.env.PORT || 3001)
