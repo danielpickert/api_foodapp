30f7ff28e933624a9f2c6d952014c757e357b042
Maseeh Es-Haq <eshaqmaseeh@yahoo.com>
2018-07-12 21:07:20 -0400

    added connection status notifications

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose')
-
+mongoose.Promise = global.Promise
 
 if (process.env.NODE_ENV === 'production') {
 	mongoose.connect(process.env.MLAB_URL);
 } else {
-	mongoose.connect('mongodb://localhost/api_foodapp')
+	mongoose.connect('mongodb://localhost/api_foodapp').then(
+		() => {
+			console.log('Connected to Mongo');
+		},
+		err => {
+			console.log('error connecting to Mongo: ')
+			console.log(err);
+		}
+	);
 }
-
 
 module.exports = mongoose
